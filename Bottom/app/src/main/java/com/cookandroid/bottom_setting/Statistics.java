10661b2abdc4833ef1ee411850ce8a056b731fca
DongHwa Kang <62324508+anima0729@users.noreply.github.com>
2021-01-21 15:28:38 +0900

    MODIFY) Statistics
    LineChart >> BarChart

--- Bottom/app/src/main/java/com/cookandroid/bottom_setting/Statistics.java
+++ Bottom/app/src/main/java/com/cookandroid/bottom_setting/Statistics.java
@@ -1,5 +1,6 @@
 package com.cookandroid.bottom_setting;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,10 +10,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
@@ -22,7 +29,6 @@ public class Statistics extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -30,28 +36,33 @@ public class Statistics extends Fragment {
         View view = inflater.inflate(R.layout.statistics, container, false);
 
         // 그래프 종류 설정
-        LineChart linechart = (LineChart) view.findViewById(R.id.chart);
+        BarChart barchart = (BarChart) view.findViewById(R.id.chart);
 
         // 그래프에 넣을 점 설정
         // 후에는 데이터베이스에서 받아온 정보를 이용하여 추가할 예정
-        ArrayList<Entry> entries = new ArrayList<>();
-        entries.add(new Entry(0, 1));
-        entries.add(new Entry(1, 3));
-        entries.add(new Entry(2, 2));
-        entries.add(new Entry(3, 1));
-        entries.add(new Entry(4, 4));
-        entries.add(new Entry(5, 0));
-        entries.add(new Entry(6, 2));
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(0, 1));
+        entries.add(new BarEntry(1, 3));
+        entries.add(new BarEntry(2, 2));
+        entries.add(new BarEntry(3, 1));
+        entries.add(new BarEntry(4, 4));
+        entries.add(new BarEntry(5, 0));
+        entries.add(new BarEntry(6, 2));
 
         // 세로축 설정
-        LineDataSet dataset = new LineDataSet(entries, "달성 목표 수");
+        BarDataSet dataset = new BarDataSet(entries, "달성 목표 수");
+        dataset.setAxisDependency(YAxis.AxisDependency.RIGHT);
 
-        LineData data = new LineData(dataset);
-        linechart.setData(data);
-        linechart.setDrawGridBackground(false);
+        // 차트에 데이터 입력
+        dataset.setColors(ColorTemplate.COLORFUL_COLORS);
+        BarData data = new BarData(dataset);
+        barchart.setData(data);
+        barchart.setDrawGridBackground(false);
+
         // 애니메이션 설정
         // 설정시 그래프가 한번에 나타나는 것이 아니라 창 이동시 그래프의 선이 제자리 찾아가면서 나타남
-        linechart.animateY(1500);
+        barchart.animateY(1500);
+        barchart.invalidate();
 
         return view;
     }
