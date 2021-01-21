c7df5e9f2847e086259b8636e31435705dcf7e9c
Sunhee Kim <76764643+sunni3722@users.noreply.github.com>
2021-01-21 17:35:26 +0900

    MODIFY) Home

--- Bottom/app/src/main/java/com/cookandroid/bottom_setting/Home.java
+++ Bottom/app/src/main/java/com/cookandroid/bottom_setting/Home.java
@@ -26,6 +26,8 @@ public class Home extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home, container, false);
+        View view = inflater.inflate(R.layout.home, container, false);
+
+        return view;
     }
 }
